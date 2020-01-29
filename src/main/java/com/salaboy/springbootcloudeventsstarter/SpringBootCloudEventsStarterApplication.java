200b929dda3df90d7b710293826f60d2560a79cc
salaboy <salaboy@gmail.com>
2020-01-29 11:09:55 +0000

    updating to v03

--- src/main/java/com/salaboy/springbootcloudeventsstarter/SpringBootCloudEventsStarterApplication.java
+++ src/main/java/com/salaboy/springbootcloudeventsstarter/SpringBootCloudEventsStarterApplication.java
@@ -1,8 +1,8 @@
 package com.salaboy.springbootcloudeventsstarter;
 
 import io.cloudevents.CloudEvent;
-import io.cloudevents.v02.AttributesImpl;
-import io.cloudevents.v02.CloudEventBuilder;
+import io.cloudevents.v03.AttributesImpl;
+import io.cloudevents.v03.CloudEventBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -29,10 +29,12 @@ public class SpringBootCloudEventsStarterApplication {
 
     @PostMapping
     public void recieveCloudEvent(@RequestHeader Map<String, String> headers, @RequestBody Object body) {
+
 		CloudEvent<AttributesImpl, String> cloudEvent = readCloudEventFromRequest(headers, body);
         System.out.println("I got a cloud event: " + cloudEvent.toString());
 		System.out.println(" -> cloud event attr: " + cloudEvent.getAttributes());
         System.out.println(" -> cloud event data: " + cloudEvent.getData());
+
     }
 
 	private CloudEvent<AttributesImpl, String> readCloudEventFromRequest(Map<String, String> headers, Object body) {
@@ -42,8 +44,7 @@ public class SpringBootCloudEventsStarterApplication {
 				.withType(headers.get("ce-type"))
 				.withSource((headers.get("ce-source")!=null)?URI.create(headers.get("ce-source")):null)
 				.withData((body != null)?body.toString():"")
-				.withContenttype(headers.get("Content-Type"))
-				.build();
+				.withDatacontenttype((headers.get("Content-Type") != null)?headers.get("Content-Type"):"application/json").build();
 	}
 
 	@GetMapping
@@ -54,7 +55,7 @@ public class SpringBootCloudEventsStarterApplication {
                 .withType("java-event")
                 .withSource(URI.create("cloudevents-java.default.svc.cluster.local"))
                 .withData("{\"name\" : \"Other From Java Cloud Event\" }")
-                .withContenttype("application/json")
+                .withDatacontenttype("application/json")
                 .build();
 		WebClient webClient = WebClient.builder().baseUrl("http://" + host ).build();
 		WebClient.RequestBodySpec uri = webClient.post().uri("/");
@@ -63,7 +64,7 @@ public class SpringBootCloudEventsStarterApplication {
 		WebClient.RequestHeadersSpec<?> header = headersSpec
 				.header("ce-id", attributes.getId())
 				.header("ce-specversion", attributes.getSpecversion())
-				.header("Content-Type", (attributes.getContenttype().isPresent())?attributes.getContenttype().get():"")
+				.header("Content-Type", (attributes.getDatacontenttype().isPresent())?attributes.getDatacontentencoding().get():"")
 				.header("ce-type", attributes.getType())
 				.header("ce-time", (attributes.getTime().isPresent())?attributes.getTime().get().toString():"")
 				.header("ce-source", (attributes.getSource()!=null)?attributes.getSource().toString():"")
